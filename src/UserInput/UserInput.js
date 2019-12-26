27a62c7665df43b0e13a35c247e0e9f00bcdb234
David Vivó <david.vivo@oramon.nl>
2019-12-26 12:23:04 +0100

    Apply inlined styles to input component

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const userInput = (props) => {
-    return (
-        <div>
-            <input type="text" onChange={props.changed} value={props.currentName} />
-        </div>
-    );
+    const inputStyle = {
+        display: 'block',
+        margin: '0 auto 1rem',
+        color: 'blue',
+        fontWeight: 'bold'
+    }
+
+    return <input 
+        type="text"
+        style={inputStyle}
+        onChange={props.changed}
+        value={props.currentName} />;
 };
+
 export default userInput;
