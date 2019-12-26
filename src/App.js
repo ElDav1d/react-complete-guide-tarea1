17403767560a8aa4ae0c9712ee6de75a5e3a4725
David Vivó <david.vivo@oramon.nl>
2019-12-26 10:51:47 +0100

    Whitespacing

--- src/App.js
+++ src/App.js
@@ -22,6 +22,9 @@ class App extends Component {
         <UserOutput username={this.state.username}/>
         <UserOutput username={this.state.username}/>
         <UserOutput username={this.state.username}/>
+        <UserOutput username={this.state.username} />
+        <UserOutput username={this.state.username} />
+        <UserOutput username={this.state.username} />
       </div>
     );
   }
