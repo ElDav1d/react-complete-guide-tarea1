b3712b3fe1bb3593f1dcdf9ca2c451ed95446419
David Vivó <david.vivo@oramon.nl>
2019-12-26 09:58:53 +0100

    Create components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello world!</h1>
+        <UserInput />
+        <UserOutput />
       </div>
     );
   }
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const userOutput = (props) => {
+    return (
+        <div>
+            <p>I'm paragraph 1</p>
+            <p>I'm paragraph 2</p>
+        </div>
+    );
+};
+export default userOutput;
