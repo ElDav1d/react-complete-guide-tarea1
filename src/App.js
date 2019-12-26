fe9e0563abdd50d5251cc444f10b46fb5054153a
David Vivó <david.vivo@oramon.nl>
2019-12-26 10:26:35 +0100

    Add username state and pass it to output component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
+  state = {
+    username: 'sameForAll'
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello world!</h1>
         <UserInput />
-        <UserOutput username="Fulano"/>
-        <UserOutput username="Mengano"/>
-        <UserOutput username="Tontano"/>
+        <UserOutput username={this.state.username}/>
+        <UserOutput username={this.state.username}/>
+        <UserOutput username={this.state.username}/>
       </div>
     );
   }
